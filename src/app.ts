f002c15f1aee0d9421952ddc1984bbe5fc69f0bc
jfpa11100 <felipepala1000@gmail.com>
2026-08-15 15:52:38 -0500

    feat: validation in environment variables 🦺

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express, { type Express, type Request, type Response } from 'express';
 
 const app: Express = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-
-app.listen(3000);
+app.listen(PORT);
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
+if (!process.env.DATABASE_URL)
+  throw new Error('DATABASE_URL environment variable is not set')
+
 const connectionString = process.env.DATABASE_URL!
 
 const client = postgres(connectionString, { prepare: false })
