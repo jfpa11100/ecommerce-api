13ee97a5f374f503a1acd233a697c75dcae5b4f2
jfpa11100 <felipepala1000@gmail.com>
2026-08-16 00:43:16 -0500

    fix: commerce routing 🩹

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express, { type Express, type Request, type Response } from 'express';
 import clientsRouter from './clients/clients.routes.ts';
+import commerceRouter from './commerce/commerce.routes.ts';
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,5 +10,6 @@ app.use(express.json())
 
 // Routes
 app.use('/clients', clientsRouter)
+app.use('/commerce', commerceRouter)
 
 app.listen(PORT);
--- src/commerce/commerce.routes.ts
+++ src/commerce/commerce.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express'
 import { commerceController } from './commerce.controller.ts'
 
-const router = Router()
+const commerceRouter = Router()
 
-router.get('/', commerceController.getAll)
-router.get('/:nit', commerceController.getByNit)
-router.query!('/search', commerceController.queryCommerce)
-router.post('/', commerceController.create)
-router.put('/:nit', commerceController.replace)
-router.patch('/:nit', commerceController.patch)
-router.delete('/:nit', commerceController.delete)
+commerceRouter.get('/', commerceController.getAll)
+commerceRouter.get('/:nit', commerceController.getByNit)
+commerceRouter.query!('/search', commerceController.queryCommerce)
+commerceRouter.post('/', commerceController.create)
+commerceRouter.put('/:nit', commerceController.replace)
+commerceRouter.patch('/:nit', commerceController.patch)
+commerceRouter.delete('/:nit', commerceController.delete)
 
-export default router
+export default commerceRouter
